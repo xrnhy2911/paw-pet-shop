430fad495840fd69f9415ff664e53f20c138bd1b
LeVy0904 <20522178@gm.uit.edu.vn>
2023-11-16 15:35:05 +0700

    first header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./header.css";
+export default function Header() {
+  return (
+    <>
+      <Router>
+        <Navbar
+          // bg="dark"
+          // variant={"dark"}
+          // expand="lg"
+          className="custom-header"
+        >
+          <Container fluid className="custom-header">
+            <Navbar.Brand href="#">Paw Pet Shop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link as={Link} to={"/"}>
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/about"}>
+                  About
+                </Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                  Link
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  id="custom-search"
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button className="custom-search-btn" variant="outline-success">
+                  Search
+                </Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        {/* Path tới các trang khác */}
+        <Routes>
+          {/* <Route path="/about">
+            <About />
+          </Route> */}
+          {/* <Route path="/">
+            <Home />
+          </Route> */}
+        </Routes>
+      </Router>
+    </>
+  );
+}
