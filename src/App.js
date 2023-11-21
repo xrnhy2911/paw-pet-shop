7142089dca914af0a32430881e080cf206556bd1
2052xxxx <20522039@gm.uit.edu.vn>
2023-11-21 15:33:21 +0700

    add font, add img
     font: nunito_sans

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './components/footer/footer';
 import Home from './components/home/Home'
 import Header from './components/header/Header';
+import Login from './components/login/Login'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -10,14 +11,16 @@ function App() {
         <Header />
         <div className='container1'></div>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           {/* <Route path="/profile" element={<Profile />} />
           <Route path="/tvshows" element={<Tvshows />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/moviesdetail" element={<MoviesDetail />} /> */}
         </Routes>
-      </Router>
       <Footer />
+      </Router>
     </div>
   );
 }
