df2c78c08943ec4025b7edcf9ed882c64c075939
LeVy0904 <20522178@gm.uit.edu.vn>
2023-11-22 13:42:24 +0700

    ProductCard #2

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -3,20 +3,26 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
-import munchkin from "../../img/munchkin_cat.jpg";
-import Container from "react-bootstrap/esm/Container";
+import image1 from "../../img/munchkin_cat.jpg";
+import image2 from "../../img/birman_1.jpg";
+import image3 from "../../img/persian_1.jpg";
+import image4 from "../../img/ragdoll_1.jpg";
+import image5 from "../../img/ragdoll_2.jpg";
+import image6 from "../../img/ragdoll_3.jpg";
+import Container from "react-bootstrap/Container";
 
 export default function ProductCard() {
+  const images = [image1, image2, image3, image4, image5, image6];
   return (
     <>
       <Container className="py-2">
         <h1 className="m-4">Our Products</h1>
         <Row xs={1} md={3} className="g-4">
-          {Array.from({ length: 12 }).map((_, idx) => (
+          {images.map((image, idx) => (
             <Col key={idx} className="mb-4">
-              <Card className="h-100 shadow">
-                <Card.Img variant="top" src={munchkin} />
-                <Card.Body>
+              <Card className="h-100 shadow d-flex flex-column">
+                <Card.Img variant="top" src={image} />
+                <Card.Body className="d-flex flex-column flex-grow-1">
                   <Card.Title>
                     <span className="h3">Card title</span>
                   </Card.Title>
@@ -26,7 +32,7 @@ export default function ProductCard() {
                     natural lead-in to additional content. This content is a
                     little bit longer.
                   </Card.Text>
-                  <Button variant="primary" className="shadow">
+                  <Button variant="primary" className="mt-auto mx-auto shadow">
                     Add to Cart
                   </Button>
                 </Card.Body>
