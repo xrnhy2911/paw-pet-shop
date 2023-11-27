beaa81a756caec34770d4cc43e19ac6b70bd0a68
2052xxxx <20522039@gm.uit.edu.vn>
2023-11-27 20:41:13 +0700

    Update home.css

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
                         <div className="location">
                             <p>Member</p>
                         </div>
-                        {/* <div className="stats">
+                        <div className="stats">
                             <div className="col-4">
                                 <h4>20</h4>
                                 <p>Reviews</p>
@@ -26,9 +26,15 @@ const Profile = () => {
                                 <h4>100</h4>
                                 <p>Discussions</p>
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                 </header>
+                <div className="proflie-content">
+                        <h2 className="heading">About Me</h2>
+                        <p>
+                            I am a software engineer who loves to learn new things. I have a passion for coding and I love to build things.
+                        </p>
+                </div>
             </section>
         </div>
     );
