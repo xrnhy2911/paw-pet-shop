8e5657ff4a07fcb813a528cb2101f0bfb10c0372
2052xxxx <20522039@gm.uit.edu.vn>
2023-11-27 14:05:55 +0700

    update profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import './profile.css'
+import React from "react";
+import "./profile.css";
+import theImg from "../../img/catlogin.jpg";
 
 const Profile = () => {
     return (
-        <div className='profile-css'>
-            <div className="picture-frame">
-                <span className="wallpaper-holder"></span>
-                <div>
-                    <span class="dot"></span>
-                </div>
-                <div className="info-frame">
-                <p>Name: John Doe</p>
-                <p>Email: johndoe@example.com</p>
-                <p>Location: New York</p>
-            </div>
-            </div>
-            
-
+        <div className="profile-css">
+            <section className="profile">
+                <header className="header">
+                    <div className="details">                        
+                        <img src={theImg} alt="" className="profile-pic" />
+                        <h1 className="heading">Claire Doe</h1>
+                        <div className="location">
+                            <p>Member</p>
+                        </div>
+                        {/* <div className="stats">
+                            <div className="col-4">
+                                <h4>20</h4>
+                                <p>Reviews</p>
+                            </div>
+                            <div className="col-4">
+                                <h4>10</h4>
+                                <p>Communities</p>
+                            </div>
+                            <div className="col-4">
+                                <h4>100</h4>
+                                <p>Discussions</p>
+                            </div>
+                        </div> */}
+                    </div>
+                </header>
+            </section>
         </div>
     );
 };
