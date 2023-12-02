233faf9b46a8d597b1792e0b52da09c06c5e4076
LeVy0904 <20522178@gm.uit.edu.vn>
2023-12-03 01:07:13 +0700

    update data

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,11 @@ import image3 from "../img/persian_1.jpg";
 import image4 from "../img/ragdoll_1.jpg";
 import image5 from "../img/ragdoll_2.jpg";
 import image6 from "../img/ragdoll_3.jpg";
-import image7 from "../img/reflex_2.jpeg";
+import image7 from "../img/bonludan.jpeg";
+import image8 from "../img/bat.jpg";
+import image9 from "../img/snappy.jpg";
+import image10 from "../img/thapbongbong.jpg";
+import image11 from "../img/vong.jpeg";
 const productDataList = [
     {
         title: "Munchkin Cat",
@@ -43,11 +47,35 @@ const productDataList = [
         productId: "6",
     },
     {
-        title: "Reflex 2",
-        price: "100,000",
+        title: "Cat Litter Box",
+        price: "345,000",
         image: image7,
         productId: "7",
-    }
+    },
+    {
+        title: "Tilted Cat Bowl",
+        price: "15,000",
+        image: image8,
+        productId: "8",
+    },
+    {
+        title: "Pate Snappy Tom",
+        price: "15,000",
+        image: image9,
+        productId: "9",
+    },
+    {
+        title: "Cat Toys",
+        price: "50,000",
+        image: image10,
+        productId: "10",
+    },
+    {
+        title: "Cat Bed Pet Hammock",
+        price: "180,000",
+        image: image11,
+        productId: "11",
+    },
 ];
 
 export { productDataList }
