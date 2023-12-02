cee705d0e3f03222a9d840f92bcbd04ef095f84b
hajmaay <20521263@gm.uit.edu.vn>
2023-12-02 23:50:13 +0700

    update product

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -13,6 +13,13 @@ import image3 from "../../img/persian_1.jpg";
 import image4 from "../../img/ragdoll_1.jpg";
 import image5 from "../../img/ragdoll_2.jpg";
 import image6 from "../../img/ragdoll_3.jpg";
+import image7 from "../../img/bonludan.jpeg";
+import image8 from "../../img/bat.jpg";
+import image9 from "../../img/snappy.jpg";
+import image10 from "../../img/thapbongbong.jpg";
+import image11 from "../../img/vong.jpeg";
+
+
 import "./productCard.css";
 
 import SearchBar from "../search-bar/SearchBar";
@@ -53,6 +60,36 @@ const productDataList = [
     price: "45,000,000",
     image: image6,
     productId: "6",
+  },
+  {
+    title: "Cat Litter Box",
+    price: "345,000",
+    image: image7,
+    productId: "7",
+  },
+  {
+    title: "Tilted Cat Bowl",
+    price: "15,000",
+    image: image8,
+    productId: "8",
+  },
+  {
+    title: "Pate Snappy Tom",
+    price: "15,000",
+    image: image9,
+    productId: "9",
+  },
+  {
+    title: "Cat Toys",
+    price: "50,000",
+    image: image10,
+    productId: "10",
+  },
+  {
+    title: "Cat Bed Pet Hammock",
+    price: "180,000",
+    image: image11,
+    productId: "11",
   },
 ];
 
