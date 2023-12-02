a79f6ffa857a4dfcd384d61283da2a7dd9a9bbca
2052xxxx <20522039@gm.uit.edu.vn>
2023-12-02 22:47:47 +0700

    update home css jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,10 @@ import chaBong1 from "../../img/chabong.jpg"
 // import chaBong1 from "../../img/chabong.jpg"
 import "./home.css";
 import Reactslider from "../slider/reactslider";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+
 
 export default function Home() {
 
@@ -25,16 +29,18 @@ export default function Home() {
           <div className="banner-container" >
             <img src={chaBong1} alt="Banner" className="banner-image" />
             <div className='hover-story'>
-              <p>There’s a cat named Caté <br /> Seeking for the pâté <br /> Loves to have a treat <br /> but hates to pay for it <br />
+              <p>There’s a cat named Caté <br /> Seeking for the pâté <br /> Loves to have a treat <br /> ... hates to pay for it <br />
 
-                Sneaks into a café <br /> Sniffs the food on display <br /> Sees a plate of pâté <br /> It’s a lucky day</p>
+                Sneaks into a café <br /> Sniffs foods on display <br /> Sees a plate of pâté <br /> It’s a lucky day</p>
 
             </div>
-            <button
+            <Button
               className="hover-button"
+              as={Link}
+              to={"/products"}
             >
               SHOP ALL
-            </button>
+            </Button>
 
 
           </div>
